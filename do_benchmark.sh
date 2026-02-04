e536db1719ab9d0714de61182a904addcbcd92b4
Herb Jellinek <jellinek@newscenter.com>
2026-02-04 12:22:16 -0800

    Added example files
    `NEW-BENCH-1`: a parameterized version of BENCH-1.  To run it, call
    `BENCH-1` with arguments TOOLS-DIR, BENCHMARKs-DIR, and RESULTS-DIR.  Example values are provided as variables `*BENCH-TOOLS-DIR*`, `*BENCH-BENCHMARKS-DIR*`, and `*BENCH-RESULTS-DIR*`.
    
    `do_benchmark.sh`: a shell script that runs Medley, sets up the environment, loads a benchmark file and executes its benchmark function.  For instance,
    
    ```bash
    $ do_benchmark.sh "{DSK}<gabriel>NEW-BENCH-1.lcom" IL:BENCH-1 "{DSK}<gabriel>tools>" "{DSK}<gabriel>" "{DSK}<gabriel>Results>r"`
    ```
    calls the benchmark `BENCH-1` from the file `{DSK}<gabriel>NEW-BENCH-1.lcom` with
    the given directory arguments.
    
    *THIS IS UNTESTED*, which is where you come in.

---
+++ do_benchmark.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+#  do_benchmark.sh
+#
+#  Script to run Gabriel benchmarks in Medley.
+#
+#  Call with the benchmark file name as the first argument and
+#  the benchmark function to call as the second argument.  For example:
+#
+#  $ do_benchmark.sh NEW-BENCH-1.lcom IL:BENCH-1 "$toolsDir" "$benchmarksDir" "$resultsDir"
+#
+#  Copyright 2026 Interlisp.org.
+#
+
+main() {
+        MEDLEYDIR=$(cd "${SCRIPTDIR}/.." && pwd)
+        export MEDLEYDIR
+        logindir=/tmp/benchmarks-$$
+        mkdir -p "${logindir}"
+        cmfile=${logindir}/hcfiles.cm
+
+	cat >"${cmfile}" <<-EOF
+	"
+
+	(PROGN
+          (IL:MEDLEY-INIT-VARS 'IL:GREET)
+	  (SETQ SYSOUTGAG T)
+	  (GCGAG 500)
+	  (IL:FILESLOAD $1)
+          (IL:SETQ IL:*UPPER-CASE-FILE-NAMES* NIL)
+          (IL:SETQ IL:NO-HELP NIL)
+	  ($2 $3 $4 $5)
+          (IL:DRIBBLE)
+          (IL:LOGOUT T)
+	)
+
+	"
+	EOF
+
+        # Make sure loadups/build is not included in HCFILES
+        if [ -d "${MEDLEYDIR}/loadups/build" ]
+        then
+          touch "${MEDLEYDIR}/loadups/build/.skip"
+        fi
+
+        /bin/sh "${MEDLEYDIR}/scripts/medley/medley.command"     \
+             --config -                                          \
+             --id bench_+                                        \
+             --geometry 1024x768                                 \
+             --noscroll                                          \
+             --logindir "${logindir}"                            \
+             --greet -                                           \
+             --rem.cm "${cmfile}"                                \
+             --apps
+
+}
+
+main "$@"
